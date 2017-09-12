6b5ff51557bf4011a03f69ca4ed0e071e9b02db2
prajwal <prajwal@mohanmurthy.com>
2017-09-12 15:33:17 +0200

    quick update

---
+++ Packages/installAll.m
@@ -0,0 +1,11 @@
+#!/usr/local/bin/wolframscript
+origin = FileNameJoin[{Directory[], "EDA"}];
+copyto = FileNameJoin[{$BaseDirectory, "Applications", "EDA"}];
+CopyDirectory[origin, copyto];
+origin = FileNameJoin[{Directory[], "CustomTicks"}];
+copyto = FileNameJoin[{$BaseDirectory, "Applications", "CustomTicks"}];
+CopyDirectory[origin, copyto];
+origin = FileNameJoin[{NotebookDirectory[], "h5mma"}];
+copyto = FileNameJoin[{$BaseDirectory, "Applications", "h5mma"}];
+CopyDirectory[origin, copyto];
+(*EOF*)
