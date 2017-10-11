3f6aaeedb2a90c1d61c8bed89ef423a7e8436247
prajwal <prajwal@mohanmurthy.com>
2017-10-11 20:18:54 +0200

    added mLaplace

---
+++ Analysis/mNStar/mTau_Laplace.m
@@ -0,0 +1,31 @@
+#!/usr/local/bin/wolframscript
+OS="mpc1636";
+If[OS=="mpc1636",
+Print["Working Directory:",curDir=SetDirectory["/home/prajwal"]];
+Print["Analysis Directory:",anDir="/home/prajwal/psi-nedm/Analysis/mNStar"];
+Print["(ASCII) Rawdata Directory:",AscDir="/xdata/nedm_data/RawData/LiveData"];
+Print["Scratch Directory: ",ScrDir="/home/prajwal/Scratch"];
+Print["Picture Directory: ",PicDir="/dev/shm/prajwal"];
+];
+ftallseries3=Import[StringJoin[anDir, "/", "ftallseries3.dat"]];
+(**)
+stt=10;
+endt=2000;
+scnftst=-N[1/(stt/Log[2])];
+scnend=-N[1/(endt/Log[2])];
+numsamples=1000000;
+scnint=Abs[scnend-scnftst]/numsamples;
+dimcctvals=Dimensions[Table[cct,{cct,scnftst,scnend,scnint}]][[1]]
+parameters=Array[a,{dimcctvals}];
+variables=Array[x,{dimcctvals}];
+guess=Table[1,{k,1,dimcctvals}];
+bb=Table[scnftst+(k*scnint),{k,1,dimcctvals}];
+f[x_,a_]:=a.Exp[x bb];
+nlmfitchk=NonlinearModelFit[ftallseries3,f[xx,parameters],Transpose[{parameters,guess}],xx];
+basechi2=Total[(nlmfitchk["FitResiduals"]/errallseries3)^2];
+nlmfitchktab=ParallelTable[Total[(NonlinearModelFit[ftallseries3,f[xx,parameters]-f[xx,Table[If[k==kk,parameters[[k]],0],{k,1,dimcctvals}]],Transpose[{parameters,guess}],xx]["FitResiduals"]/errallseries3)^2],{kk,1,dimcctvals}];
+Export["mTau_Laplace_info.dat",{stt,endt,numsamples,basechi2,"\n"}];
+(**)
+ptnlmfitchktab=Table[{-Log[2]/bb[[k]],Log[nlmfitchktab[[k]]]},{k,1,dimcctvals}];
+Export["mTau_Laplace_fit.dat",ptnlmfitchktab];
+(*EOF*)
